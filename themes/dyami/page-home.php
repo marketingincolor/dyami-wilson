439f3b83db158b54223673bc6532396e4fe60b10
nbeers22 <nbeers22@gmail.com>
2017-10-27 13:31:23 -0400

    Began home page and set up some scss templates

--- themes/dyami/page-home.php
+++ themes/dyami/page-home.php
@@ -3,5 +3,130 @@
 	get_header();
 ?>
 
+	<section class="home-hero" style="background-image: url('/wp-content/uploads/2017/10/home-hero-b.png');background-position: center center;background-size:cover">
+		<div class="row">
+			<div class="medium-8 medium-centered columns text-center">
+				<h1 class="fff">Passionate About Music &amp; Sound Design</h1>
+				<ul>
+					<li class="fff">Musician</li> &bull;
+					<li class="fff">Engineer</li> &bull;
+					<li class="fff">Sound Designer</li> &bull;
+					<li class="fff">Producer</li>
+				</ul>
+				<a href="<?php echo site_url().'/contact'; ?>"><button class="btn-white">Contact Dyami</button></a>
+			</div>
+		</div>
+	</section>
+
+	<section class="featured-work">
+		<div class="row">
+			<div class="small-12 columns clearfix">
+				<h2>Featured Work</h2>
+				<p><a href="#!"><button class="btn-black">View All Projects</button></a></p>
+			</div>
+			<div class="clearfix"></div>
+			<div class="medium-5 large-4 columns clearfix">
+				<h3>Android</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi dolores, repudiandae soluta similique voluptate? Perferendis harum, unde architecto molestiae! Autem tenetur quas fugit ut voluptates et natus laborum, ex ad!</p>
+				<p><a href="#!" class="text-uppercase">View Project</a></p>
+			</div>
+			<div class="medium-6 large-7 medium-offset-1 columns">
+				<img src="http://fillmurray.com/800/400" alt="">
+			</div>
+			<div class="medium-6 large-7 medium-offset-1 columns">
+				<img src="http://fillmurray.com/800/400" alt="">
+			</div>
+			<div class="medium-5 large-4 columns">
+				<h3>Android</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi dolores, repudiandae soluta similique voluptate? Perferendis harum, unde architecto molestiae! Autem tenetur quas fugit ut voluptates et natus laborum, ex ad!</p>
+				<p><a href="#!" class="text-uppercase">View Project</a></p>
+			</div>
+		</div>
+	</section>
+
+	<section class="services">
+		<div class="row">
+			<div class="small-12 columns clearfix">
+				<h2 class="fff">Services</h2>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+		<div class="row expanded columns" style="background-image:url(<?php bloginfo('template_directory'); ?>/assets/images/home/services-top-bg.jpg);">
+			<aside class="clearfix">
+				<div class="medium-9 columns service-box">
+					<div class="row">
+						<div class="medium-9 columns">
+							<h3>Mixing</h3> <a href="#!">View Projects</a>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore laborum ullam voluptatibus optio culpa, neque fuga amet, magnam cupiditate minus, error nulla sequi iusto, asperiores adipisci facilis ab quod animi!</p>
+						</div>
+					</div>
+				</div>
+			</aside>
+		</div>
+		<div class="row expanded columns" style="background-image:url(<?php bloginfo('template_directory'); ?>/assets/images/home/services-bottom-bg.jpg);">
+			<aside class="clearfix">
+				<div class="medium-6 medium-offset-4 columns service-box end">
+					<h3>Producing</h3> <a href="#!">View Projects</a>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati numquam ut veniam saepe accusantium eos explicabo, voluptatem laboriosam. Unde sed fugiat harum labore hic eveniet repellendus culpa, natus? Doloribus, modi?</p>
+				</div>
+			</aside>
+		</div>
+	</section>
+
+	<section class="services2">
+		<div class="row expanded columns">
+			<img src="<?php bloginfo('template_directory'); ?>/assets/images/home/services-full-img.jpg" alt="">
+		</div>
+		<div class="row">
+			<div class="medium-5 medium-offset-1 columns">
+				<div class="service-box">
+					<h3>Sound Design</h3> 
+					<a href="#!">View Projects</a>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati numquam ut veniam saepe accusantium eos explicabo, voluptatem laboriosam. Unde sed fugiat harum labore hic eveniet repellendus culpa, natus? Doloribus, modi?</p>
+				</div>
+			</div>
+			<div class="medium-5 columns end">
+				<div class="service-box">
+					<h3>Composing</h3>
+					<a href="#!">View Projects</a>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati numquam ut veniam saepe accusantium eos explicabo, voluptatem laboriosam. Unde sed fugiat harum labore hic eveniet repellendus culpa, natus? Doloribus, modi?</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="about">
+		<div class="row">
+			<div class="medium-11 medium-offset-1 columns">
+				<h2>Meet Dyami</h2>
+			</div>
+			<div class="medium-11 medium-offset-1 columns relative">
+				<div class="row">
+					<div class="medium-8 columns end">
+						<div class="row">
+							<div class="medium-11 columns">
+								<p>Dyami Wilson has been creating music and sound since 2004. He started playing guitar at age 13 and has studied and produced music for the last 12 years. Examples of records he has played on can be found here:</p>
+								<p>Glo “From the Inside” 2017  Mixing, producing, guitar, songwriting</p>
+								<p>Flux Forteana “September” 2015  Guitar</p>
+								<p>Ry Mohon “Not Like the Bread” 2015  Background vocals, production</p>
+								<p>He recently earned his degree in electronic production and sound design from Berklee College of Music, and currently works as a producer, engineer, and sound designer.</p>
+								<p>Along with his passion for playing guitar, in his free time Dyami enjoys snowboarding, hiking, running, and meeting, talking, and laughing with people.</p>
+							</div>
+						</div>
+						<aside class="testimonial-box">
+							<div class="row">
+								<div class="medium-11 columns">
+									<p><i class="fa fa-quote-left" aria-hidden="true"></i></p>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum quas accusamus tempora nisi illum, debitis voluptatem impedit officiis. Voluptates odio molestias dolores tempore neque sed, assumenda recusandae laudantium consequatur culpa.</p>
+									<p class="author">&mdash; Testimonial Author</p>
+								</div>
+							</div>
+						</aside>
+					</div>
+					<img src="<?php bloginfo('template_directory'); ?>/assets/images/home/dyami-wilson.jpg" alt="Dyami Wilson">
+				</div>
+			</div>
+		</div>
+	</section>
 
 <?php get_footer(); ?>
