3acbb077b06a856151f9f5039572e2b6103c1d0e
Adam Doe <adoe@marketingincolor.com>
2017-10-30 17:01:53 -0400

    Updated the header

---
+++ themes/dyami/assets/javascript/custom/scroll.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	$(window).on('scroll', function() {
+
+		var depth = $(this).scrollTop();
+		var scrolled = false;
+	    console.log( $(this).scrollTop() );
+
+	    if(depth > 500 && scrolled == false ) {
+	    	$('#hamburger').stop(true,true).removeClass('hamburger');
+	    	$('#hamburger').stop(true,true).addClass('hamburger--scrolling');
+	    	scrolled = true;
+	    }
+
+	    else {
+	    	$('#hamburger').stop(true,true).addClass('hamburger');
+	    	$('#hamburger').stop(true,true).removeClass('hamburger--scrolling');	    	
+	    }
+
+
+	});
+
+})
--- themes/dyami/header.php
+++ themes/dyami/header.php
@@ -27,6 +27,9 @@
 	<?php do_action( 'foundationpress_layout_start' ); ?>
 
 	<header class="site-header" role="banner">
+
+		<!-- HIDE FOR NOW -->
+		<?php if(false): ?>
 		<?php get_template_part('page-templates/hamburger-icon'); ?>
 		<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle() ?>>
 			<div class="title-bar-left">
@@ -36,6 +39,58 @@
 				</span>
 			</div>
 		</div>
+		<?php endif; ?>
+
+
+		<nav class="site-navigation top-bar" role="navigation">
+
+			<div class="row expanded top-bar__wrapper" data-equalizer>
+
+				<!-- Hamburger Menu -->
+				<div class="small-3 columns top-bar__hamburger-menu" data-equalizer-watch>
+					<?php get_template_part('page-templates/hamburger-icon'); ?>
+					<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle() ?>>
+						<div class="title-bar-left">
+							<button class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
+							<span class="site-mobile-title title-bar-title">
+								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+							</span>
+						</div>
+					</div>
+				</div>
+				<!-- /Hamburger Menu -->
+
+				<!-- Equalizer Icon -->
+				<div class="small-6 columns top-bar__equalizer-icon" data-equalizer-watch>
+					<a href="<?php site_url(); ?>" title="Home"><img src="<?php echo get_stylesheet_directory_uri() .'/assets/images/icons/logo-white.png'; ?>" alt="Equalizer Icon"></a>
+				</div>
+				<!-- /Equalizer Icon -->
+				
+				<!-- Social Icons -->
+				<div class="small-3 columns text-center top-bar__social-icons" data-equalizer-watch>
+					<ul class="">
+						<li><a href="#!" title="Facebook Profile"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
+						<li><a href="#!" title="Twitter Profile"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+						<li><a href="#!" title="Spotify Profile"><i class="fa fa-spotify" aria-hidden="true"></i></a></li>
+						<li><a href="#!" title="SoundCloud"><i class="fa fa-soundcloud" aria-hidden="true"></i></a></li>
+					</ul> 
+				</div>
+				<!-- /Social Icons -->
+
+			</div>
+
+			<!-- Name Logo -->
+			<div class="row">
+				<div class="small-12 columns text-center top-bar__name">
+					<img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/dyami-wilson-logo.png'; ?>" alt="Dyami Wilson Logo" />
+				</div>
+			</div>
+			<!-- /Name Logo -->
+
+		</nav>
+
+		<!-- HIDE THIS FOR NOW. -->
+		<?php if(false): ?>
 		<nav class="site-navigation top-bar" role="navigation">
 			<div class="top-bar-left">
 				<div class="site-desktop-title top-bar-title">
@@ -50,7 +105,9 @@
 				<?php endif; ?>
 			</div>
 		</nav>
+		<?php endif; ?>
 	</header>
+	<!-- /HIDE THIS FOR NOW. -->
 
 	<section class="container clearfix">
 		<?php do_action( 'foundationpress_after_header' );
