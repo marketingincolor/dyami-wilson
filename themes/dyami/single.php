3ed859dff795cbe0d3236566081e288a1d39ed76
nbeers22 <nbeers22@gmail.com>
2017-11-01 10:50:25 -0400

    minor changes

--- themes/dyami/single.php
+++ themes/dyami/single.php
@@ -1,48 +1,43 @@
-<?php
-/**
- * The template for displaying all single posts and attachments
- *
- * @package FoundationPress
- * @since FoundationPress 1.0.0
- */
+<?php get_header(); ?>
 
-get_header(); ?>
-
-<?php get_template_part( 'template-parts/featured-image' ); ?>
-
-<div class="main-wrap" role="main">
-
-<?php do_action( 'foundationpress_before_content' ); ?>
 <?php while ( have_posts() ) : the_post(); ?>
-	<article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
-		<header>
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-			<?php foundationpress_entry_meta(); ?>
-		</header>
-		<?php do_action( 'foundationpress_post_before_entry_content' ); ?>
-		<div class="entry-content">
-			<?php the_content(); ?>
-			<?php edit_post_link( __( 'Edit', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
+
+	<section class="home-hero" style="background: url( <?php bloginfo('template_directory'); ?>/assets/images/single/single-hero.jpg) center center no-repeat;background-size:cover"></section>
+
+	<section class="featured-post">
+		<div class="row">
+			<div class="large-6 medium-8 columns medium-centered">
+				<!-- Featured image -->
+				<div class="feat-img-cont" style="background: url(<?php the_post_thumbnail_url(); ?>) center center no-repeat;background-size:cover;">
+
+					<?php if(has_post_format('audio')) { ?>
+
+						<div class="play">
+							<p><i class="fa fa-volume-up" aria-hidden="true"></i></p>
+							<p>Play Audio</p>
+						</div>
+
+					<?php }else if(has_post_format('video')) { ?>
+
+						<div class="play">
+							<p><i class="fa fa-play-circle" aria-hidden="true"></i></p>
+							<p>Play Video</p>
+						</div>
+
+					<?php } ?>
+
+				</div>
+				<!-- Post Title/Content -->
+				<header>
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+				</header>
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div>
+			</div>
 		</div>
-		<footer>
-			<?php
-				wp_link_pages(
-					array(
-						'before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ),
-						'after'  => '</p></nav>',
-					)
-				);
-			?>
-			<p><?php the_tags(); ?></p>
-		</footer>
-		<?php the_post_navigation(); ?>
-		<?php do_action( 'foundationpress_post_before_comments' ); ?>
-		<?php comments_template(); ?>
-		<?php do_action( 'foundationpress_post_after_comments' ); ?>
-	</article>
+	</section>
+
 <?php endwhile;?>
 
-<?php do_action( 'foundationpress_after_content' ); ?>
-<?php get_sidebar(); ?>
-</div>
 <?php get_footer();
