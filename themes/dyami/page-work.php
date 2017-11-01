7c2d137ba2a118e81f6ff4c1e65d9dc2c0b4e1a8
nbeers22 <nbeers22@gmail.com>
2017-11-01 12:49:41 -0400

    Finished up basic skelton and functionality of single page. Split up into templates

--- themes/dyami/page-work.php
+++ themes/dyami/page-work.php
@@ -15,13 +15,7 @@
 	<div id="work-container"></div>
 </section>
 
-<!-- Video/Audio Modal -->
-<div class="reveal" id="audio-video-modal" data-reveal>
-  <h1 id="modal-title"></h1>
-  <div id="modal-body"></div>
-  <button class="close-button" data-close aria-label="Close modal" type="button">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>
+<!-- Include Modal -->
+<?php require('page-templates/audio-video-modal.php') ?>
 
 <?php endwhile;endif;get_footer(); ?>
