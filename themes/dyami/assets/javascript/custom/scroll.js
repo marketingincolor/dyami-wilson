c651101ad4506fe625da09bf81e92102cb83ec3d
Adam Doe <adoe@marketingincolor.com>
2017-11-01 12:41:44 -0400

    Menu updates

--- themes/dyami/assets/javascript/custom/scroll.js
+++ themes/dyami/assets/javascript/custom/scroll.js
@@ -1,21 +1,21 @@
-$(document).ready(function() {
-	$(window).on('scroll', function() {
+// $(document).ready(function() {
+// 	$(window).on('scroll', function() {
 
-		var depth = $(this).scrollTop();
-		var scrolled = false;
+// 		var depth = $(this).scrollTop();
+// 		var scrolled = false;
 
-	    if(depth > 500 && scrolled == false ) {
-	    	$('#hamburger').stop(true,true).removeClass('hamburger');
-	    	$('#hamburger').stop(true,true).addClass('hamburger--scrolling');
-	    	scrolled = true;
-	    }
+// 	    if(depth > 0 && scrolled == false ) {
+// 	    	$('#hamburger').stop(true,true).removeClass('hamburger');
+// 	    	$('#hamburger').stop(true,true).addClass('hamburger--scrolling');
+// 	    	scrolled = true;
+// 	    }
 
-	    else {
-	    	$('#hamburger').stop(true,true).addClass('hamburger');
-	    	$('#hamburger').stop(true,true).removeClass('hamburger--scrolling');	    	
-	    }
+// 	    else {
+// 	    	$('#hamburger').stop(true,true).addClass('hamburger');
+// 	    	$('#hamburger').stop(true,true).removeClass('hamburger--scrolling');	    	
+// 	    }
 
 
-	});
+// 	});
 
-})
+// })
